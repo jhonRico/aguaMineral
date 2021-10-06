0b9492168bdb27628a5109a0387da7bb71b9ec1c
LeydiCAvellanedaG <lcavellanedag@gmail.com>
2021-10-06 18:55:48 -0500

    12 Commit Jeffry

--- vistas/js/contratoEstante.js
+++ vistas/js/contratoEstante.js
@@ -1,6 +1,197 @@
-$(".boton1").click(function(){
+$(function(){
+     $(".boton1").click(function(){
+      var nombre = $('#nombreCliente').val();
+      var apellido = $('#apellidoCliente').val();
+      var direccion = $('#direccion').val();
+      var comercio = $("#nobreComercio").val();
+      var cedula = $('#cedulaCliente').val();
+      var telefono = $('#telefono').val();
+      var direccionComercio = $('#direccionComercio').val();
+      var cantidadEstantes = $('#inputCity').val();
+
+      if (nombre == "") 
+      {
+         Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese un nombre',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (!expresiones.nombre.test(nombre)) 
+      {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese un nombre válido',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+
+      if (apellido == '') 
+      {
+       Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese un apellido',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+
+      if (!expresiones.nombre.test(apellido)) 
+      {
+         Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese un apellido válido',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (cedula == '') 
+      {
+         Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese una cédula',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (!expresiones.telefono.test(cedula)) 
+      {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese una cedula válida',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (direccion == '') 
+      {
+         Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese un dirección',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (comercio == '') 
+      {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese un comercio',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (!expresiones.nombre.test(comercio)) 
+      {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese un comercio válido',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (telefono == '') 
+      {
+         Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese una telefono',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (!expresiones.telefono.test(telefono)) 
+      {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese un teléfono válido',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (direccionComercio == '') 
+      {
+         Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese una dirección',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      if (cantidadEstantes == '') 
+      {
+         Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          toast: true,
+          title: 'Ingrese una cantidad',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 1500
+        })        
+        return false;
+      }
+      mostrarModal();
+      //RegistrarUsuario();
+
+      return true;
+
+      });
+ });
+
+function mostrarModal(){
+
    $("#modal2").modal("show");
-})
+}
 
 $(function(){
 	$(".cerrar").click(function(){
--- vistas/modulos/contratoEstante.php
+++ vistas/modulos/contratoEstante.php
@@ -11,11 +11,12 @@ if(isset($_SESSION["id"])){
 }
 
 $variable = '</br></br>
-<div class="ms-5 me-5 mb-3 text-end fst-italic">
+<div class="ms-5 me-5 mb-3 fst-italic">
 <h class="justificadoTotal">
 <img src="http://localhost/aguaMineral/vistas/img/general/logoTransparente.JPG" class="h-25 w-25">
 <p class="titulo">CONTRATO DE ESTANTERIA</p>
 </br></br>
+<p class="justify-text fs-6">
 Entre nosotros, Jesus Abel Chacon Zambrano,  ingeniero, mec&aacute;nico,
 venezolano soltero, mayor de edad, domiciliado en la ciudad d Palmira,
 municipio guasimos del estado T&aacute;chira,  titular de la cedula de identidad
@@ -41,45 +42,46 @@ la cantidad de (____________), estante, de estructura, met&aacutelica, para ser
 utilizado como exhibidor de botellones contentivos de agua mineral para 
 consumo humano, el cual es de la UNICA Y EXCLUSIVA PROPIEDAD DEL EL COMODANTE
 y as&iacute continuara siendo durante la vigencia del presente contrato. CLAUSULA 
-SEGUNDA: a la estructura met&aacutelica aquí otorgada en comodato y que el adelante
+SEGUNDA: a la estructura met&aacutelica aqu&iacute otorgada en comodato y que el adelante
 denominaremos EL ESTANTE, para los efectos del presente contrato, con capacidad
 para albergar y transportar (________) unidades conocidas como botellones, se 
-identifica con el código serial(es) Nº _____________, nuevo o reconstruido de 
-fecha de construcción (_______________) cuenta con un letrero identificatorio 
-del producto AGUA MINERAL UREÑA, marca que EL COMODANTE distribuye de forma 
-exclusiva. Las unidades  o envases denominadas botellones y el líquido en ellas 
-contenido, forman parte de una transacción comercial separada e independiente del
+identifica con el c&oacutedigo serial(es) Nº _____________, nuevo o reconstruido de 
+fecha de construcci&oacute (_______________) cuenta con un letrero identificatorio 
+del producto AGUA MINERAL URE&NtildeA, marca que EL COMODANTE distribuye de forma 
+exclusiva. Las unidades  o envases denominadas botellones y el l&iacutequido en ellas 
+contenido, forman parte de una transacci&oacuten comercial separada e independiente del
 presente contrato. CLAUSULA TERCERA: EL COMODATARIO se obliga a utilizar EL ESTANTE
 que recibe en comodato, UNICAMENTE, para almacenar, exhibir y vender, productos 
 de las marcas producidas o comercializadas por EL COMODANTE, independientemente 
 de quien ostente la propiedad o derechos similares del sitio donde se instala, 
-perfectamente identificado en le clausula primera de este contrato, también se 
+perfectamente identificado en le clausula primera de este contrato, tambi&iacuten se 
 obliga EL COMODATARIO a hacerle mantenimiento preventivo a EL ESTANTE y a permitirle a EL COMODANTE,
-o quien el designe, para hacer inspecciones periódicas verificando que el
+o quien el designe, para hacer inspecciones peri&oacutedicas verificando que el
 mismo se haga. CLAUSULA CUARTA: EL COMODATARIO se compromete a no utilizar
 EL ESTANTE para garantizar obligaciones personales o comerciales que asuma
-o pueda asumir, bajo ningún concepto o situación y en cuanto se presente
-una situación personal, legal, privada o pública, EL COMODATARIO se obliga
-a dejar claramente sentado el titulo mediante el cual usufructúa EL ESTANTE
-y quien es su legítimo propietario. CLAUSULA QUINTA: EL COMODATARIO acepta
-que, si llegara  a incumplir cualesquiera de las cláusulas del presente
+o pueda asumir, bajo ning&uacuten concepto o situaci&iocuten y en cuanto se presente
+una situaci&oacuten personal, legal, privada o p&uacuteblica, EL COMODATARIO se obliga
+a dejar claramente sentado el titulo mediante el cual usufruct&uacutea EL ESTANTE
+y quien es su leg&iicutetimo propietario. CLAUSULA QUINTA: EL COMODATARIO acepta
+que, si llegara  a incumplir cualesquiera de las cl&aacuteusulas del presente
 contrato o de los anexos que el mismo pueda tener a futuro, reconocidos por
-las partes, dará derecho AL COMODANTE para retirar EL ESTANTE del sitio donde
-el mismo se encuentre, sin que EL COMODATARIO pueda alegar  su favor ningún
+las partes, dar&aacute derecho AL COMODANTE para retirar EL ESTANTE del sitio donde
+el mismo se encuentre, sin que EL COMODATARIO pueda alegar  su favor ning&uacuten
 derecho sobre el mismo o los bienes que albergue EL ESTANTE  en este momento.
-Así mismo, acepta EL COMODATARIO asumir bajo su única y exclusiva responsabilidad,
-todos los gastos, costos y costas, daños y deterioro causados a EL ESTANTE,
+As&iacute mismo, acepta EL COMODATARIO asumir bajo su &uacutenica y exclusiva responsabilidad,
+todos los gastos, costos y costas, da&ntildeos y deterioro causados a EL ESTANTE,
 necesarios o presentes para recuperarlo por cualquiera de los motivos contemplados
 o referidos en la presente clausula o en el cuerpo del presente contrato.
 CLAUSULA SEXTA: Es claro, definitivo y aceptado entre las partes que EL COMODANTE
-no vende ni negocia EL ESTANTE  bajo ningún concepto, pero, si en el ejercicio
+no vende ni negocia EL ESTANTE  bajo ning&uacuten concepto, pero, si en el ejercicio
 del presente COMODATO otorgado a EL COMODATARIO, el mismo llegara a perderse,
 no poder ser inspeccionado o evaluado por EL COMODANTE o la persona , personas
 o instituciones que le autorice para hacerlo, se establece, como valor referencial
 del mismo, la cantidad de CINCUENTA (50) UNIDADES TRIBUTARIAS, por cada casilla
-(espacio) del ESTANTE,  que EL COMODATARIO deberá cancelar a EL COMODANTE, en dinero
-en efectivo, al requerimiento que este último le haga. Sin embargo. Así lo decimos
-y otorgamos, en la fecha de la nota respectiva. 
+(espacio) del ESTANTE,  que EL COMODATARIO deber&aacute cancelar a EL COMODANTE, en dinero
+en efectivo,</p><br><p class="text-start fs-6"> al requerimiento que este &uacuteltimo le haga.Sin embargo. As&iacute lo decimos
+y otorgamos, en la fecha de la nota respectiva.</p>
+
 
 </br></br></br></br>
 ________________________</br>
@@ -98,8 +100,12 @@ COMODATARIO               &nbsp;&nbsp;&nbsp;&nbsp;         DPTO DE VENTAS
 ?>
 
 <div class="Home ms-5"></div>
-  <div class="container-fluid well well-sm barraProductos p-5 text-center col-md-10">
-    <h1 class="tituloContratos text-dark me-5">Crear contrato estante</h1>
+  <div class="container-fluid well well-sm barraProductos p-5 text-center col-md-6">
+    <div class="me-5">
+       <div class="me-5">
+        <h1 class="tituloContratos text-dark me-5">Crear contrato estante</h1>
+       </div>
+    </div>
   </div>
   <div class="container-fluid p-3 ms-5" >
     <div class="container ms-5">
@@ -122,8 +128,8 @@ COMODATARIO               &nbsp;&nbsp;&nbsp;&nbsp;         DPTO DE VENTAS
 
         <div class="row">
           <div class="form-group col-md-6">
-            <label for="inputEmail4" class="mt-3">Cedula del cliente</label>
-            <input type="text" class="form-control mt-3" id="nombreCliente" placeholder="Nombre">
+            <label for="inputEmail4" class="mt-3">C&eacute;dula del cliente</label>
+            <input type="text" class="form-control mt-3" id="cedulaCliente" placeholder="C&eacute;dula">
           </div>
           <div class="form-group col-md-6">
             <label for="inputState" class="mt-3">Municipio de residencia</label>
@@ -149,7 +155,7 @@ COMODATARIO               &nbsp;&nbsp;&nbsp;&nbsp;         DPTO DE VENTAS
           <input type="text" class="form-control mt-3" id="nobreComercio" placeholder="Nombre del comercio">
         </div>
         <div class="form-group col-md-6">
-          <label for="inputPassword">T&eacute;lefono del comercio</label>
+          <label for="inputPassword" class="mt-3">T&eacute;lefono del comercio</label>
           <input type="text" class="form-control mt-3" id="telefono" placeholder="T&eacute;lefono del comercio">
         </div>
 
@@ -214,7 +220,7 @@ VENTANA MODAL PARA EL REGISTRO
         $dia = date("l", strtotime($fecha));
         $mes = date("F", strtotime($fecha));
         $anio = date("Y", strtotime($fecha));
-        $dias_ES = array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
+        $dias_ES = array("Lunes", "Martes", "Mi&eacutercoles", "Jueves", "Viernes", "S&aacutebado", "Domingo");
         $dias_EN = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
         $nombredia = str_replace($dias_EN, $dias_ES, $dia);
         $meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");

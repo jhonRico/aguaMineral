39227544164917eb20ad55110f06279aa70da3cd
admin <admin@text.com>
2021-10-29 22:34:23 -0500

    cambios

--- vistas/modulos/adminFormatoBotellones.php
+++ vistas/modulos/adminFormatoBotellones.php
@@ -9,7 +9,7 @@
   <div class="container mt-3 fs-5 ms-5">
         <nav aria-label="breadcrumb" class="ms-5">
           <ol class="breadcrumb" class="ms-5">
-            <li class="breadcrumb-item"><a href="http://localhost/aguaMineral/adminFormatos" class="link-primary">Administración de Ubicación</a></li>
+            <li class="breadcrumb-item"><a href="http://localhost/aguaMineral/adminFormatos" class="link-primary">Administración de formatos</a></li>
             <li class="breadcrumb-item active" aria-current="page" class="">Formatos</li>
           </ol>
         </nav>
--- vistas/modulos/adminFormatos.php
+++ vistas/modulos/adminFormatos.php
@@ -16,7 +16,7 @@ MOSTRAR UNA LISTA DE 6 BLOGS POR CADA PAGINA
  <div class="container mt-3 fs-5">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="http://localhost/aguaMineral/administracion" class="link-primary">Administración</a></li>
+            <li class="breadcrumb-item"><a href="http://localhost/aguaMineral/administracion" class="link-primary">Administración Contrato</a></li>
             <li class="breadcrumb-item active" aria-current="page" class="">Administración de Formato</li>
           </ol>
         </nav>
